8f23beb050aa502dd70a3e9dd9dfa839c6b14dae
Luise Kalus <luise.kalus@gmail.com>
2021-11-24 17:25:18 +0000

    Adding star data to the package

---
+++ R/tutorials_data.R
@@ -0,0 +1,10 @@
+#' STAR: Project STAR: Student-Teacher Achievement Ratio
+#'
+#' The Project STAR public access data set, assessing the effect of reducing class size on test scores in the early grades.
+#'
+#' @format A data frame with 2161 rows and 48 variables:
+#' \describe{ }
+#'
+#'
+#' @source \url{https://www.rdocumentation.org/packages/AER/versions/1.2-9/topics/STAR}
+"diamonds"
